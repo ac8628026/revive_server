7621237511edb093aba6c2013bcd3b3f60f31c28
ac8628026 <ac8628026@gmail.com>
2026-01-05 11:21:16 +0530

    fix port issue

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,18 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config();
 
+console.log("ABOUT TO LISTEN ON PORT", process.env.PORT);
+
+
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://revive.kgpian.site"
+  ],
+  credentials: true
+}));
+
 
 app.use(express.json())
 app.use("/v0/api",routes)
@@ -15,25 +25,28 @@ app.use("/v0/api",routes)
 
 const db = mongoose.connection
 
-db.on("connected",()=>{console.log("mongo connected")})
-
 db.on("error",(e)=>{console.log("mongo connection error",e)})
 
 db.on("disconnected",()=>{console.log("mongo disconnected")})
 
-async function startServer(){
+async function startServer() {
+  try {
+    if (!process.env.DB_URI) {
+      throw new Error("DB URI is not found in env");
+    }
 
-    try{
-        if(!process.env.DB_URI){
-            throw new Error("DB URI is not found in env")
-        }
-        await mongoose.connect(process.env.DB_URI)
-        console.log("mongo connected")
-        app.listen(3000,()=>console.log("server is runing"))
-    }
-    catch(e){
-        console.error(e)
-    }
+    await mongoose.connect(process.env.DB_URI);
+    console.log("mongo connected");
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log("server is running on port", PORT);
+    });
+  } catch (e) {
+    console.error("startup error:", e);
+    process.exit(1); 
+  }
 }
 
-startServer()
+startServer();
